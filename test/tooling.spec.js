e825eab11248017df63e8013c9800836c8f8daef
Sebastian Herrlinger <sherrlinger@groupon.com>
2016-04-27 20:31:05 +0200

    Add tooling specs

---
+++ test/tooling.spec.js
@@ -0,0 +1,14 @@
+/**
+ * Tests
+ */
+/* eslint-disable new-cap */
+const expect = require('expect.js');
+// const sinon = require('sinon');
+const cannot = require('../');
+const semver = require('semver');
+
+describe('Tooling', () => {
+  //
+  //
+  it('has a pre-commit hook to check if the specs changed and update them for the commit');
+});
